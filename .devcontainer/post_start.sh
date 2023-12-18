408b932c2d153cb0582df90fa2be334d94c4b53e
tomo1227 <tomo.soccer1227@gmail.com>
2023-12-18 12:01:52 +0000

    💚 feat: terraformのインストール

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,6 +1,6 @@
 #  ~/.gitconfigの設定
-echo "copy local ~./gitconfig"
-git config --global --add safe.directory ${containerWorkspaceFolder}
+echo "copy local ~/.gitconfig"
+git config --global --add safe.directory /workspace
 
 git config pull.rebase false
 git config --global commit.template .commit_template
