8e56dc27835b7562be1f8132bc411168fc665b2e
tomo1227 <tomo.soccer1227@gmail.com>
2024-01-08 15:58:38 +0000

    chore:♻️ 不要な箇所を削除

--- internal/infrastructure/database/mysql/mysql.go
+++ internal/infrastructure/database/mysql/mysql.go
@@ -19,37 +19,3 @@ func start() {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 }
-
-// CreateUser 関数を呼び出してユーザーを作成する
-// ctx := context.Background()
-// _, err = CreateUser(ctx, client)
-// if err != nil {
-// 	log.Fatalf("failed creating user: %v", err)
-// }
-
-// func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
-// 	u, err := client.User.
-// 		Create().
-// 		SetAge(30).
-// 		SetName("a8m").
-// 		Save(ctx)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed creating user: %w", err)
-// 	}
-// 	log.Println("user was created: ", u)
-// 	return u, nil
-// }
-
-// func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
-// 	u, err := client.User.
-// 		Query().
-// 		Where(user.Name("a8m")).
-// 		// `Only` fails if no user found,
-// 		// or more than 1 user returned.
-// 		Only(ctx)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed querying user: %w", err)
-// 	}
-// 	log.Println("user returned: ", u)
-// 	return u, nil
-// }
